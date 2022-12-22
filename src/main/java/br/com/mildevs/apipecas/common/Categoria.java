bbfd40ef531d9c8683a75e732c77348d7f0ad6e0
Axionauta <daniln2209@gmail.com>
2022-12-22 02:03:15 -0300

    feat: adiciona atributo enum

--- src/main/java/br/com/mildevs/apipecas/common/Categoria.java
+++ src/main/java/br/com/mildevs/apipecas/common/Categoria.java
@@ -1,3 +1,5 @@
+package br.com.mildevs.apipecas.common;
+
 public enum Categoria {
   FUNILARIA,
   MOTOR,
--- src/main/java/br/com/mildevs/apipecas/entity/Peca.java
+++ src/main/java/br/com/mildevs/apipecas/entity/Peca.java
@@ -1,14 +1,18 @@
 package br.com.mildevs.apipecas.entity;
 
+import br.com.mildevs.apipecas.common.Categoria;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 
 @Entity
 public class Peca {
-  String nome;
-  String modeloCarro;
-  String fabricante;
-  float precoCusto;
-  float precoVenda;
-  int quantidadeEstoque;
-  
+  private String nome;
+  private String modeloCarro;
+  private String fabricante;
+  private float precoCusto;
+  private float precoVenda;
+  private int quantidadeEstoque;
+  @Enumerated(EnumType.STRING)
+  private Categoria categoria;
 }
