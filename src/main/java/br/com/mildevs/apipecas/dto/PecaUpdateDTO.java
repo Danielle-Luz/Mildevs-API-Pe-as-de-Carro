c91142967699e6b187e1e524be8504e6de39173a
Axionauta <daniln2209@gmail.com>
2022-12-22 17:08:07 -0300

    feat: adiciona dto update com getters e construtor

--- src/main/java/br/com/mildevs/apipecas/dto/PecaUpdateDTO.java
+++ src/main/java/br/com/mildevs/apipecas/dto/PecaUpdateDTO.java
@@ -1,5 +1,30 @@
 package br.com.mildevs.apipecas.dto;
 
 public class PecaUpdateDTO {
-  
+
+  private float precoCusto;
+  private float precoVenda;
+  private int quantidadeEstoque;
+
+  public PecaUpdateDTO(
+    float precoCusto,
+    float precoVenda,
+    int quantidadeEstoque
+  ) {
+    this.precoCusto = precoCusto;
+    this.precoVenda = precoVenda;
+    this.quantidadeEstoque = quantidadeEstoque;
+  }
+
+  public float getPrecoCusto() {
+    return precoCusto;
+  }
+
+  public float getPrecoVenda() {
+    return precoVenda;
+  }
+
+  public int getQuantidadeEstoque() {
+    return quantidadeEstoque;
+  }
 }
