ca6f33f228d4eccedace136908512c0c0281a421
Axionauta <daniln2209@gmail.com>
2022-12-22 17:02:03 -0300

    feat: adiciona getters e setters no dto create

--- src/main/java/br/com/mildevs/apipecas/dto/PecaCreateDTO.java
+++ src/main/java/br/com/mildevs/apipecas/dto/PecaCreateDTO.java
@@ -12,19 +12,70 @@ public class PecaCreateDTO {
   private int quantidadeEstoque;
   private Categoria categoria;
 
+  public PecaCreateDTO(String nome, String modeloCarro, String fabricante, float precoCusto, float precoVenda,
+      int quantidadeEstoque, Categoria categoria) {
+    this.nome = nome;
+    this.modeloCarro = modeloCarro;
+    this.fabricante = fabricante;
+    this.precoCusto = precoCusto;
+    this.precoVenda = precoVenda;
+    this.quantidadeEstoque = quantidadeEstoque;
+    this.categoria = categoria;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getModeloCarro() {
+    return modeloCarro;
+  }
+
+  public void setModeloCarro(String modeloCarro) {
+    this.modeloCarro = modeloCarro;
+  }
+
+  public String getFabricante() {
+    return fabricante;
+  }
+
+  public void setFabricante(String fabricante) {
+    this.fabricante = fabricante;
+  }
+
   public float getPrecoCusto() {
     return precoCusto;
+  }
+
+  public void setPrecoCusto(float precoCusto) {
+    this.precoCusto = precoCusto;
   }
 
   public float getPrecoVenda() {
     return precoVenda;
   }
 
+  public void setPrecoVenda(float precoVenda) {
+    this.precoVenda = precoVenda;
+  }
+
   public int getQuantidadeEstoque() {
     return quantidadeEstoque;
   }
 
+  public void setQuantidadeEstoque(int quantidadeEstoque) {
+    this.quantidadeEstoque = quantidadeEstoque;
+  }
+
   public Categoria getCategoria() {
     return categoria;
+  }
+
+  public void setCategoria(Categoria categoria) {
+    this.categoria = categoria;
   }
 }
