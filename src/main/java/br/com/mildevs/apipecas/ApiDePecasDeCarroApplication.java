6d7e45049f693104543961cb5d1501e134e4d2cb
Axionauta <daniln2209@gmail.com>
2022-12-22 00:34:32 -0300

    feat: add initial files

---
+++ src/main/java/br/com/mildevs/apipecas/ApiDePecasDeCarroApplication.java
@@ -0,0 +1,13 @@
+package br.com.mildevs.apipecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDePecasDeCarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDePecasDeCarroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mildevs/apipecas/ApiDePecasDeCarroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mildevs.apipecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDePecasDeCarroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
