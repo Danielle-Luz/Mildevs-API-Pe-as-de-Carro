7fdae60e945efc9615b03e609bf33254a0c1a583
Axionauta <daniln2209@gmail.com>
2022-12-22 18:02:45 -0300

    feat: adiciona método de criação de peça

--- src/main/java/br/com/mildevs/apipecas/service/PecaService.java
+++ src/main/java/br/com/mildevs/apipecas/service/PecaService.java
@@ -1,12 +1,39 @@
 package br.com.mildevs.apipecas.service;
 
+import br.com.mildevs.apipecas.dto.PecaCreateDTO;
+import br.com.mildevs.apipecas.entity.PecaEntity;
+import br.com.mildevs.apipecas.error.NumeroNegativoException;
+import br.com.mildevs.apipecas.repository.PecaRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.mildevs.apipecas.entity.PecaEntity;
-
 @Service
 public class PecaService {
+
   @Autowired
-  PecaEntity entity;
+  PecaRepository repository;
+
+  public PecaCreateDTO criaPeca(PecaCreateDTO novaPeca)
+    throws NumeroNegativoException, IllegalArgumentException {
+    if (novaPeca.getPrecoCusto() < 0) {
+      throw new NumeroNegativoException(
+        "O preço de custo deve ser um valor maior ou igual a zero"
+      );
+    } else if (novaPeca.getPrecoVenda() < 0) {
+      throw new NumeroNegativoException(
+        "O preço de venda deve ser um valor maior ou igual a zero"
+      );
+    } else if (novaPeca.getQuantidadeEstoque() < 0) {
+      throw new NumeroNegativoException(
+        "A quantidade em estoque deve ser um valor maior ou igual a zero"
+      );
+    }
+
+    PecaEntity pecaEntity = new PecaEntity();
+    BeanUtils.copyProperties(novaPeca, pecaEntity);
+    repository.save(pecaEntity);
+
+    return novaPeca;
+  }
 }
