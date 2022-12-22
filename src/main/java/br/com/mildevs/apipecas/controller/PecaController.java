1a93bd3d56fab0afa98aa9f18d86de9df320dd24
Axionauta <daniln2209@gmail.com>
2022-12-22 20:51:38 -0300

    feat: adiciona requisição para criar peça

--- src/main/java/br/com/mildevs/apipecas/controller/PecaController.java
+++ src/main/java/br/com/mildevs/apipecas/controller/PecaController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.mildevs.apipecas.dto.PecaCreateDTO;
 import br.com.mildevs.apipecas.dto.PecaGetResponseDTO;
 import br.com.mildevs.apipecas.service.PecaService;
 
@@ -31,4 +33,11 @@ public class PecaController {
   public List<PecaGetResponseDTO> buscarPelaCategoria(@PathVariable String categoria) {
     return service.buscarPecaPelaCategoria(categoria);
   }
+
+  @PostMapping
+  public PecaCreateDTO criarPeca(@RequestBody PecaCreateDTO novaPeca) {
+    return service.criaPeca(novaPeca);
+  }
+
+  
 }
