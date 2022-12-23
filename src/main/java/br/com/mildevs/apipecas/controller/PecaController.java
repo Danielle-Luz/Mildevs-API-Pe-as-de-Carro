9243a2ae4852c6636d2f536996740a947bcf15c3
Axionauta <daniln2209@gmail.com>
2022-12-22 23:22:39 -0300

    feat: adiciona response body no tratamento de exceções

--- src/main/java/br/com/mildevs/apipecas/controller/PecaController.java
+++ src/main/java/br/com/mildevs/apipecas/controller/PecaController.java
@@ -1,7 +1,12 @@
 package br.com.mildevs.apipecas.controller;
 
+import br.com.mildevs.apipecas.dto.PecaCreateDTO;
+import br.com.mildevs.apipecas.dto.PecaGetResponseDTO;
+import br.com.mildevs.apipecas.dto.PecaUpdateDTO;
+import br.com.mildevs.apipecas.error.NumeroNegativoException;
+import br.com.mildevs.apipecas.service.PecaService;
 import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,39 +15,45 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.mildevs.apipecas.dto.PecaCreateDTO;
-import br.com.mildevs.apipecas.dto.PecaGetResponseDTO;
-import br.com.mildevs.apipecas.dto.PecaUpdateDTO;
-import br.com.mildevs.apipecas.service.PecaService;
-
 @RestController
 @RequestMapping("/pecas")
 public class PecaController {
+
   @Autowired
   PecaService service;
 
   @GetMapping(path = "{texto}/comeco")
-  public List<PecaGetResponseDTO> buscarPecaPeloNome(@PathVariable String texto) {
+  public List<PecaGetResponseDTO> buscarPecaPeloNome(
+    @PathVariable String texto
+  ) {
     return service.buscaPecaPorNome(texto);
   }
 
   @GetMapping(path = "{modelo}/modelo")
-  public List<PecaGetResponseDTO> buscarPecaPeloModeloCarro(@PathVariable String modelo) {
+  public List<PecaGetResponseDTO> buscarPecaPeloModeloCarro(
+    @PathVariable String modelo
+  ) {
     return service.buscarPecaPeloModeloCarro(modelo);
   }
 
   @GetMapping(path = "{categoria}/categoria")
-  public List<PecaGetResponseDTO> buscarPelaCategoria(@PathVariable String categoria) {
+  public List<PecaGetResponseDTO> buscarPelaCategoria(
+    @PathVariable String categoria
+  ) {
     return service.buscarPecaPelaCategoria(categoria);
   }
 
   @PostMapping
-  public PecaCreateDTO criarPeca(@RequestBody PecaCreateDTO novaPeca) {
+  public PecaCreateDTO criarPeca(@RequestBody PecaCreateDTO novaPeca)
+    throws NumeroNegativoException, IllegalArgumentException {
     return service.criaPeca(novaPeca);
   }
 
   @PatchMapping(path = "/{idPecaAtualizada}")
-  public PecaUpdateDTO atualizarPeca(@RequestBody PecaUpdateDTO pecaAtualizada, @PathVariable long idPecaAtualizada) {
+  public PecaUpdateDTO atualizarPeca(
+    @RequestBody PecaUpdateDTO pecaAtualizada,
+    @PathVariable long idPecaAtualizada
+  ) throws NumeroNegativoException {
     return service.atualizaPeca(pecaAtualizada, idPecaAtualizada);
   }
 }
