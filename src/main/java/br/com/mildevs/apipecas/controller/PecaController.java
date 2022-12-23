6f2fbfa1ff176553d99405c5f971f3d8d1d97b00
Axionauta <daniln2209@gmail.com>
2022-12-23 00:00:48 -0300

    feat: adiciona response status na requisição de criação

--- src/main/java/br/com/mildevs/apipecas/controller/PecaController.java
+++ src/main/java/br/com/mildevs/apipecas/controller/PecaController.java
@@ -6,6 +6,7 @@ import br.com.mildevs.apipecas.dto.PecaUpdateDTO;
 import br.com.mildevs.apipecas.error.NumeroNegativoException;
 import br.com.mildevs.apipecas.service.PecaService;
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -44,6 +46,7 @@ public class PecaController {
   }
 
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
   public PecaCreateDTO criarPeca(@RequestBody PecaCreateDTO novaPeca)
     throws NumeroNegativoException, IllegalArgumentException {
     return service.criaPeca(novaPeca);
